86f07ff913ffe99fd6f053eabd4310f860d45df7
soroarbd <soroar42@gmail.com>
2025-05-14 09:53:02 +0600

    Added S3 bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "Statics_Web" {
+    bucket = var.bucket_name
+
+    tags = {
+      Name = "Statics_Web"
     }
-  }
-  
+}
+
+
+
+
+
--- terrafrom.tfvars
+++ terrafrom.tfvars
@@ -1,1 +1,2 @@
 aws_region = "us-east-1"
+bucket_name = "Static_web_SSS"
